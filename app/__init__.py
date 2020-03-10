0e39ce5957dae75e51d8171720412c40bd754d2d
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-03-10 14:47:17 -0300

    add adduser endpoint, migrate for user model and password_hash

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,41 @@
-from app import app
+from app import app, db, request, jsonify, generate_password_hash
+from app.models import User
 
 @app.route('/')
 def hello():
     return 'hello'
+
+@app.route('/adduser')
+def add_user():
+    try:
+        if not request.is_json:
+            raise Exception('Request must be in json format')
+
+        username = request.json.get('username')
+        password = request.json.get('password')
+        email = request.json.get('email')
+        
+        if not username or not password or not email:
+            raise Exception('Request must contain all required fields.')
+        
+        user_exist = User.query.filter(User.username == username).first() is not None
+        if user_exist:
+            raise Exception("User already exist")
+
+        email_has_user = User.query.filter(User.email == email).first() is not None
+        if email_has_user:
+            raise Exception("E-mail already has a user")
+        
+        password_hash = generate_password_hash(password)
+        db.session.add(User(username=username, email=email, password=password_hash))
+        db.session.commit()
+
+        response = jsonify("User saved with success")
+        response.status_code = 200
+
+        return response
+    except Exception as error:
+        response = jsonify("{0}".format(str(error)))
+        response.status_code = 400
+        return response
+        
