2f6fd1a3b760a0336197b1bdce97eda116464bd9
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-03-10 17:59:32 -0300

    Add jwt and login

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,35 @@
-from app import app, db, request, jsonify, generate_password_hash, jwt_required, create_access_token, get_jwt_identity
+from app import app, db, request, jsonify, generate_password_hash, jwt_required, create_access_token, get_jwt_identity, check_password_hash
 from app.models import User
 
 @app.route('/')
 def hello():
     return 'hello'    
 
-@app.route('/adduser')
+@app.route('/login', methods=['GET'])
+def login():
+    try:
+        if not request.is_json:
+            raise Exception("Request must be in json format")
+
+        user_login = request.get_json()
+        if 'username' not in user_login or 'password' not in user_login:
+            raise Exception("Request must have all the required fields")
+        
+        user = User.query.filter(User.username == user_login['username']).first()
+        if not user or not check_password_hash(user.password, user_login['password']):
+            raise Exception('Invalid username or password')
+        
+        response = jsonify({'msg': 'User logged with success', 'token': create_access_token(identity=user_login['username'])})
+        response.status_code = 200
+        
+        return response
+    except Exception as error:
+        response = jsonify("{0}".format(str(error)))
+        response.status_code = 400
+        return response
+    
+
+@app.route('/adduser', methods=['POST'])
 @jwt_required
 def add_user():
     try:
