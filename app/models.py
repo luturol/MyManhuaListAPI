068a6677c5127a7e66d1567cdcbc93e6f5f20ab6
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-03-24 15:01:20 -0300

    Add mangas and first access to login

--- app/models.py
+++ app/models.py
@@ -5,6 +5,17 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128))
+    first_access = db.Column(db.String(1))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Manga(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
+    name = db.Column(db.String(256))
+    chapter = db.Column(db.Integer)
+    state = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
